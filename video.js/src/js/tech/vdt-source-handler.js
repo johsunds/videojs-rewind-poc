735ec2ec08272d4e3898c1fe1afa6999eb3db96f
Johan Sundström <jsundstrom1994@gmail.com>
2023-06-14 21:11:15 +0200

    vdt source handler test

--- video.js/src/js/tech/vdt-source-handler.js
+++ video.js/src/js/tech/vdt-source-handler.js
@@ -3,14 +3,21 @@
 
 class VdtHandler {
 
+    // Default segment size in bytes
     static DEFAULT_SEGMENT_SIZE = 1 * (2 ** 20);
 
+    // The next segment will be buffered when we are `BUFFER_NEXT_SEGMENT_THRESHOLD` seconds
+    // real time (i.e. accounting for playbackRate) away from playing it.
+    static BUFFER_NEXT_SEGMENT_THRESHOLD = 5;
+
     constructor(source, tech) {
-        console.log({ source, tech });
         this.source = source;
         this.tech = tech;
         this.mediaSource = new MediaSource();
         this.mediaSource.addEventListener('sourceopen', this.sourceOpen.bind(this));
+        this.mediaSource.addEventListener('sourceclose', () => {
+            console.log("SOURCECLOSE");
+        });
         this.tech.src(URL.createObjectURL(this.mediaSource));
     }
 
@@ -28,64 +35,107 @@ class VdtHandler {
     initSegmentsList(sourceSize) {
         const segmentSize = Math.min(VdtHandler.DEFAULT_SEGMENT_SIZE, sourceSize);
         const nSegments = Math.ceil(sourceSize / segmentSize);
-        console.log({nSegments, segmentSize, sourceSize, test: VdtHandler.DEFAULT_SEGMENT_SIZE})
         this.segmentList = [...new Array(nSegments)].map((_, i) => {
             return {
                 byteStart: i * segmentSize,
-                byteEnd: Math.min((i + 1) * segmentSize, sourceSize),
+                byteEnd: Math.min((i + 1) * segmentSize, sourceSize) - 1,
                 requested: false,
             }
         })
     }
 
     async sourceOpen() {
-        console.log("SOURCEOPEN");
         this.sourceBuffer = this.mediaSource.addSourceBuffer(this.source.type);
-        this.sourceBuffer.mode = "segments";
+        this.sourceBuffer.mode = "sequence";
         this.sourceSize = await this.getSourceSize();
         this.initSegmentsList(this.sourceSize);
         this.bufferIfNeeded();
         this.tech.on("timeupdate", this.bufferIfNeeded.bind(this));
-        this.tech.el().addEventListener("canplay", () => {
-            console.log({duration: this.tech.duration() })
+        this.tech.on('seeking', this.seek.bind(this));
+        this.sourceBuffer.addEventListener("updateend", () => {
+            console.log("UPDATE END")
+            // this.sourceBuffer.timestampOffset = 5;
         });
-        //   video.addEventListener('seeking', seek);
     }
 
     currentSegmentIndex() {
-        if (!this.segmentDuration) return 0;
-        return Math.min(Math.trunc(this.tech.currentTime() / this.segmentDuration), this.segmentList.length);
+        if (!this.segmentDuration()) return 0;
+        return Math.min(Math.trunc(this.tech.currentTime() / this.segmentDuration()), this.segmentList.length);
     }
 
-    async bufferIfNeeded() {
-        const currentSegmentIndex = this.currentSegmentIndex();
-        const currentSegment = this.segmentList[currentSegmentIndex];
-        console.log("BUFFER IF NEEDED")
-        if (currentSegmentIndex === 0 && !currentSegment.requested) {
-            this.buffer(0);
+    segmentDuration() {
+        const duration = this.tech.duration();
+        return duration ? (duration / this.segmentList.length) : undefined;
+    }
+
+    getSegmentTimeRange(segment) {
+        if (!this.tech.duration()) return undefined;
+        const start = (segment.byteStart / this.sourceSize) * this.tech.duration();
+        const end = (segment.byteEnd / this.sourceSize) * this.tech.duration();
+        return {
+            start,
+            end,
         }
     }
 
-    async buffer(segmentIndex) {
-        const segment = this.segmentList[segmentIndex];
-        console.log("BUFFER", segment);
+    nextSegmentIndex() {
+        const currentSegmentIndex = this.currentSegmentIndex();
+        const nextSegmentIndex = currentSegmentIndex + (this.tech.playbackRate() > 0 ? 1 : -1);
+        if (nextSegmentIndex < 0 || nextSegmentIndex >= this.segmentList.length) {
+            return undefined;
+        }
+        return nextSegmentIndex;
+    }
+
+    bufferIfNeeded() {
+        const currentSegmentIndex = this.currentSegmentIndex();
+        const currentSegment = this.segmentList[currentSegmentIndex];
+
+        if (!currentSegment.requested) {
+            return this.buffer(currentSegment);
+        }
+
+        const playbackRate = this.tech.playbackRate();
+        const currentTime = this.tech.currentTime();
+        const nextSegmentIndex = this.nextSegmentIndex();
+
+        if (nextSegmentIndex === undefined) return;
+
+        const nextSegment = this.segmentList[nextSegmentIndex];
+        const nextSegmentTimeRange = this.getSegmentTimeRange(nextSegment);
+        const nextSegmentBegin = playbackRate > 0 ? nextSegmentTimeRange.start : nextSegmentTimeRange.end;
+        const timeUntilNextSegment = (nextSegmentBegin - currentTime) / playbackRate;
+
+        if (!nextSegment.requested && timeUntilNextSegment < VdtHandler.BUFFER_NEXT_SEGMENT_THRESHOLD) {
+            this.buffer(nextSegment);
+        }
+    }
+
+    buffer(segment) {
         segment.requested = true;
-        var xhr = new XMLHttpRequest;
+        const segmentTimeRange = this.getSegmentTimeRange(segment);
+        console.log("BUFFER", { segment, buffer: this.sourceBuffer, segmentTimeRange });
+        const xhr = new XMLHttpRequest;
         xhr.open('get', this.source.src);
         xhr.responseType = 'arraybuffer';
+        console.log("HEADER", 'bytes=' + segment.byteStart + '-' + segment.byteEnd);
         xhr.setRequestHeader('Range', 'bytes=' + segment.byteStart + '-' + segment.byteEnd);
         xhr.onload = () => {
-          console.log("fetched segment");
-          console.log(segment);
+          console.log("APPENDING");
+          if (segmentTimeRange) {
+            this.sourceBuffer.timestampOffset = segmentTimeRange.start;
+            console.log({timestampOffset: this.sourceBuffer.timestampOffset})
+          }
           this.sourceBuffer.appendBuffer(xhr.response);
         };
         xhr.send();
-
     }
 
     seek() {
-        if (mediaSource.readyState === 'open') {
+        if (this.mediaSource.readyState === 'open') {
+          console.log("ABORT");
           this.sourceBuffer.abort();
+          this.bufferIfNeeded();
         }
     }
 
